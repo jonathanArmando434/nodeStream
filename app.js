9141f5c5490bb74f0b69a76b6ba4b31e8a6f46f4
jonathanarmando434 <jonathanarmando434@gmail.com>
2025-05-01 17:34:36 +0100

    implement video streaming with range support and add HTML player

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-import { createServer }  from 'node:http';
-import { join } from 'node:path';
+import { createServer }  from 'node:http'
+import { join } from 'node:path'
+import fs from 'node:fs'
 
-const app = createServer((req, res) => {
+const app = createServer(async (req, res) => {
     const dirname = import.meta.dirname;
-    const videoPath = join(dirname, 'cap.america.mp4');
+    const videoPath = join(dirname, 'cap.america.mp4')
+    const {size} = await fs.promises.stat(videoPath)
+    const {range} = req.headers
 
-    console.log(videoPath);
+    if(!range) {
+        res.writeHead(200, {
+            'content-length': size,
+            'Content-Type': 'video/mp4'
+        })
 
-    res.end(videoPath);
+        return fs.createReadStream(videoPath).pipe(res)
+    }
+
+    const chunkSize = 1024 * 1024 // 1MB
+    const [startStr, endStr] = range.replace(/bytes=/, '').split('-')
+    const start = parseInt(startStr, 10)
+    const end = endStr ? parseInt(endStr, 10) : Math.min(start + chunkSize - 1, size - 1)
+
+    res.writeHead(206, {
+        'Content-Range': `bytes ${start}-${end}/${size}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': end - start + 1,
+        'Content-Type': 'video/mp4'
+    })
+
+    const stream = fs.createReadStream(videoPath, {start, end})
+    return stream.pipe(res)
 })
 
 app.listen(3000, () => {
